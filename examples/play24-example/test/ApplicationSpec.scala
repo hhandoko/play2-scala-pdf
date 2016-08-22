5d1cff16694adba7cd5aa03565f48c220f7c69af
Herdy Handoko <herdy.handoko@gmail.com>
2016-08-22 10:10:02 +0800

    Added simple test cases for example application

--- examples/play24-example/test/ApplicationSpec.scala
+++ examples/play24-example/test/ApplicationSpec.scala
@@ -1,12 +1,36 @@
+/**
+ * File     : ApplicationSpec.scala
+ * License  :
+ *   The MIT License (MIT)
+ *
+ *   Original   - Copyright (c) 2014 Jöerg Viola, Marco Sinigaglia
+ *   Derivative - Copyright (c) 2016 Citadel Technology Solutions Pte Ltd
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ */
 import org.scalatestplus.play._
+
 import play.api.test._
 import play.api.test.Helpers._
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
+import com.builtamont.controllers.routes
+
 class ApplicationSpec extends PlaySpec with OneAppPerTest {
 
   "Routes" should {
@@ -20,11 +44,26 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
   "HomeController" should {
 
     "render the index page" in {
-      val home = route(app, FakeRequest(GET, "/")).get
+      val Some(home) = route(app, FakeRequest(GET, routes.HomeController.index().url))
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
       contentAsString(home) must include ("Hello, world!")
+    }
+
+    "render the example page as HTML" in {
+      val Some(exampleHtml) = route(app, FakeRequest(GET, routes.HomeController.exampleHtml().url))
+
+      status(exampleHtml) mustBe OK
+      contentType(exampleHtml) mustBe Some("text/html")
+      contentAsString(exampleHtml) must include ("Example page for `play2-scala-pdf`")
+    }
+
+    "render the example page as PDF" in {
+      val Some(exampleHtml) = route(app, FakeRequest(GET, routes.HomeController.examplePdf().url))
+
+      status(exampleHtml) mustBe OK
+      contentType(exampleHtml) mustBe Some("application/pdf")
     }
 
   }
--- examples/play24-example/test/IntegrationSpec.scala
+++ examples/play24-example/test/IntegrationSpec.scala
@@ -1,20 +1,57 @@
-import org.scalatestplus.play._
-import play.api.test._
-import play.api.test.Helpers._
-
 /**
- * add your integration spec here.
- * An integration test will fire up a whole play application in a real (or headless) browser
+ * File     : IntegrationSpec.scala
+ * License  :
+ *   The MIT License (MIT)
+ *
+ *   Original   - Copyright (c) 2014 Jöerg Viola, Marco Sinigaglia
+ *   Derivative - Copyright (c) 2016 Citadel Technology Solutions Pte Ltd
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
  */
+import org.scalatestplus.play._
+
+import com.builtamont.controllers.routes
+
 class IntegrationSpec extends PlaySpec with OneServerPerTest with OneBrowserPerTest with HtmlUnitFactory {
+
+  val BASE_URL = "http://localhost:" + port
 
   "Application" should {
 
     "work from within a browser" in {
-
-      go to ("http://localhost:" + port)
+      go to BASE_URL
 
       pageSource must include ("Hello, world!")
     }
+
+    "load example page as HTML" in {
+      go to (BASE_URL + routes.HomeController.exampleHtml().url)
+
+      pageTitle must include ("`play2-scala-pdf` Example | Example Page")
+      pageSource must include ("Example page for `play2-scala-pdf`")
+    }
+
+    "load example page as PDF" in {
+      go to (BASE_URL + routes.HomeController.examplePdf().url)
+
+      pageSource must include ("`play2-scala-pdf` Example | Example Page")
+    }
+
   }
 }
