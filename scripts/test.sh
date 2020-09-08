e308ab82837bc8f9545b6806b22900391f60a3f6
Herdy Handoko <herdy.handoko@gmail.com>
2020-09-09 07:55:57 +0800

    Bump copyright year

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,7 @@
 # License  :
 #   The MIT License (MIT)
 #
-#   Copyright (c) 2016 - 2019 play2-scala-pdf Contributors
+#   Copyright (c) 2016 - 2020 play2-scala-pdf Contributors
 #
 #   Permission is hereby granted, free of charge, to any person obtaining a copy
 #   of this software and associated documentation files (the "Software"), to deal
